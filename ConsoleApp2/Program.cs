a41fb26cacf95be01b085354e62c9a63ddc8d923
Andrei Epure <andrei.epure@sonarsource.com>
2023-11-06 18:48:48 +0100

    add console project

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+using ConsoleApp2;
+
+Console.WriteLine("Hello, World!");
+
+var c = new class1();
+
+Console.WriteLine(1);
+
